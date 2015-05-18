7e10d83e04b8b982777969c7847d5ec19c8f6550
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-05-18 09:39:05 -0300

    Producers para serializers Model sobreescrito

--- src/main/java/br/eti/clairton/vraptor/crud/hypermedia/HypermediableProducer.java
+++ src/main/java/br/eti/clairton/vraptor/crud/hypermedia/HypermediableProducer.java
@@ -1,18 +1,32 @@
 package br.eti.clairton.vraptor.crud.hypermedia;
 
+import static javax.enterprise.inject.spi.CDI.current;
+
+import java.lang.annotation.Annotation;
 import java.util.Collection;
 
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.Specializes;
+import javax.enterprise.util.AnnotationLiteral;
 
+import net.vidageek.mirror.dsl.Mirror;
 import br.eti.clairton.inflector.Inflector;
 import br.eti.clairton.repository.Model;
+import br.eti.clairton.vraptor.crud.serializer.Producer;
 import br.eti.clairton.vraptor.hypermedia.HypermediableRule;
 import br.eti.clairton.vraptor.hypermedia.Operation;
 import br.eti.clairton.vraptor.hypermedia.Resource;
 
 import com.google.gson.JsonSerializer;
 
-public class HypermediableProducer {
+public class HypermediableProducer extends Producer {
+	private final Annotation oQ = new AnnotationLiteral<Operation>() {
+		private static final long serialVersionUID = 1L;
+	};
+	private final Annotation rQ = new AnnotationLiteral<Resource>() {
+		private static final long serialVersionUID = 1L;
+	};
+
 	@Produces
 	public JsonSerializer<Collection<Model>> getSerializerCollection(
 			final HypermediableRule navigator,
@@ -22,12 +36,15 @@ public class HypermediableProducer {
 				inflector);
 	}
 
+	@Override
 	@Produces
-	public JsonSerializer<Model> getSerializer(
-			final HypermediableRule navigator,
-			final @Operation String operation, final @Resource String resource,
-			Inflector inflector) {
-		return new ModelSerializer(navigator, operation, resource);
+	@Specializes
+	public JsonSerializer<Model> serializer(Mirror mirror) {
+		final Class<HypermediableRule> t = HypermediableRule.class;
+		final HypermediableRule navigator = current().select(t).get();
+		final String operation = current().select(String.class, oQ).get();
+		final String resource = current().select(String.class, rQ).get();
+		return new ModelSerializer(navigator, resource, operation);
 	}
 
 }
