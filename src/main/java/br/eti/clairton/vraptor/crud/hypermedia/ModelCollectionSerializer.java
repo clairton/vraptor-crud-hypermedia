3302f9056c0100687448f4daf3541f444828fbb5
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-09-11 14:12:14 -0300

    Links in Empty PaginatedList

--- src/main/java/br/eti/clairton/vraptor/crud/hypermedia/ModelCollectionSerializer.java
+++ src/main/java/br/eti/clairton/vraptor/crud/hypermedia/ModelCollectionSerializer.java
@@ -5,20 +5,25 @@ import java.lang.reflect.Type;
 import java.util.Collection;
 
 import javax.annotation.Priority;
+import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
-
+import br.com.caelum.vraptor.controller.BeanClass;
+import br.com.caelum.vraptor.controller.ControllerMethod;
 import br.eti.clairton.gson.hypermedia.Hypermediable;
 import br.eti.clairton.gson.hypermedia.HypermediableCollection;
 import br.eti.clairton.gson.hypermedia.HypermediableCollectionSerializer;
 import br.eti.clairton.gson.hypermedia.HypermediableRule;
 import br.eti.clairton.inflector.Inflector;
+import br.eti.clairton.paginated.collection.PaginatedCollection;
 import br.eti.clairton.repository.Model;
+import br.eti.clairton.security.Extractor;
 import br.eti.clairton.vraptor.crud.serializer.Resourceable;
 import br.eti.clairton.vraptor.crud.serializer.TagableExtractor;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 @Priority(0)
 public class ModelCollectionSerializer extends HypermediableCollectionSerializer<Model> implements JsonSerializer<Collection<Model>>, HypermediableCollection<Model>, Serializable, Resourceable<Collection<Model>> {
@@ -56,7 +61,20 @@ public class ModelCollectionSerializer extends HypermediableCollectionSerializer
 	
 	@Override
 	protected Boolean isResource(final Collection<Model> src) {
-		return (!src.isEmpty() && Model.class.isInstance(getFirst(src))) && getRootTag(getFirst(src)).equals(getResource(src));
+		return (!src.isEmpty() && Model.class.isInstance(getFirst(src))) && getRootTag(getFirst(src)).equals(getResource(src)) 
+					|| (src.isEmpty() && PaginatedCollection.class.isInstance(src)  && getResource().equals(getResource(src)) );
+	}
+
+	private String getResource() {
+		try{
+			final BeanClass beanClass = CDI.current().select(ControllerMethod.class).get().getController();
+			final Object controller = beanClass.getType().newInstance();
+			final Extractor extractor = CDI.current().select(Extractor.class).get();
+			final String resource = extractor.getResource(controller);
+			return resource;
+		}catch(final Exception e){
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
